874c6189b48d6afab52bf90d05c82333fb0a9fdf
marina-nakagiri <84775885+marina-nakagiri@users.noreply.github.com>
2022-05-30 18:58:19 +0900

    23

--- src/index.js
+++ src/index.js
@@ -112,3 +112,33 @@
 // const [name, age] = myProfile;
 // const message4 = `私の名前は${name}です。年齢は${age}歳です。`;
 // console.log(message4);
+
+/**
+ *  デフォルト値、引数など
+ *
+//  */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
+// sayHello();
+
+/**
+ *  スプレッド構文　ドット３つ
+ *
+ */
+
+//配列の展開
+// const arr1 = [1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+
+// sumFunc(...arr1);
+
+//まとめる
+
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
